026c474353ef78791ff3b1777356fb3dc44e3081
SashalmiImre <sashalmi.imre@gmail.com>
2014-08-15 12:58:32 +0200

    Debug

---
+++ Assets/MyCamera.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MyCamera : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.LogWarning ("Kamera init", this);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
